74407c24b7f8abd87671a482c3a25785439c2c83
Floris den Heijer <floris@denheijer.com>
2018-03-08 16:01:19 +0100

    Package update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import OverlayLogo from "./Logo";
+import ndjsonStream from "can-ndjson-stream";
 import "./App.css";
 
 /*
@@ -22,9 +23,41 @@ List upcoming block
 
 */
 
-class PushListener extends Component {
-	
+function makeRequest() {
+	return { 
+		method: 'GET', 
+		headers: {
+			'Authorization': 'Basic '+btoa('roeivierkamp:C47FG5MxUdJR7MmZ'),
+			'User-Agent': 'H4KScreen'
+		}};
 }
+
+function listenPush() {
+  fetch("/some/endpoint") // make a fetch request to a NDJSON stream service
+    .then(response => {
+      return ndjsonStream(response.body); //ndjsonStream parses the response.body
+    })
+    .then(exampleStream => {
+      //retain access to the reader so that you can cancel it
+      const reader = exampleStream.getReader();
+      let read;
+
+      reader.read().then(
+        (read = result => {
+          if (result.done) {
+            return;
+          }
+          console.log(result.value); //logs {item:"first"}
+          exampleStream
+            .getReader()
+            .read()
+            .then(read);
+        })
+      );
+    });
+}
+
+class PushListener extends Component {}
 
 class App extends Component {
   render() {
@@ -36,7 +69,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit :::D <code>src/App.js</code> and save to reload.
         </p>
-				<OverlayLogo text="Roeivierkamp 2018" />
+        <OverlayLogo text="Roeivierkamp 2018" />
       </div>
     );
   }
