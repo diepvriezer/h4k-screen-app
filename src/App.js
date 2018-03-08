ddcbe914995ec71cd09f581bc85237df01bc31b6
Floris den Heijer <floris@denheijer.com>
2018-03-08 06:02:08 +0100

    API explorations

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import OverlayLogo from "./Logo";
 import "./App.css";
+
+/*
+Race in progress?
+Y: listen for results, show block
+N: wait for "space"
+
+On space:
+List upcoming block
+ - Saturday 2500m/250m Block 1
+  * Coming up
+		* Show divisions
+		* 43 DCl 8+ (40 entries)
+		* 48 ... (n entries)
+	
+ - Race mode
+  * Listen for start
+	* 
+
+
+*/
+
+class PushListener extends Component {
+	
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit :::D <code>src/App.js</code> and save to reload.
         </p>
-				<p>
-					<OverlayLogo text="Roeivierkamp 2018" />
-				</p>
+				<OverlayLogo text="Roeivierkamp 2018" />
       </div>
     );
   }
